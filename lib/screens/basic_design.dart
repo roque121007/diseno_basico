6c8354d6f37dc653ed943407391d6ecefadb5175
roque121007 <jesusalexanderroquetrejo90@gmail.com>
2025-06-03 05:56:02 -0600

    mejorar diseño básico añadiendo secciones de botones y texto descriptivo

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -10,8 +10,52 @@ class BasicDesignScreen extends StatelessWidget {
         children: [
           Image(image: AssetImage("assets/istockphoto-1097050568-612x612.jpg")),
           Title(),
+          ButtonSection(),
+
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: Text(
+              "Veniam minim cillum nisi adipisicing fugiat sunt nisi dolor. Nisi magna proident qui fugiat pariatur nostrud nostrud culpa cupidatat cillum duis voluptate eu. Nostrud consequat commodo ullamco laborum magna velit mollit ad adipisicing eiusmod exercitation. Sint veniam quis sit nisi minim. Et laboris dolor aute ut laborum culpa veniam fugiat aliqua ullamco.",
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButtom(icon: Icons.call, text: "Call"),
+          CustomButtom(icon: Icons.navigation, text: "Route"),
+          CustomButtom(icon: Icons.share, text: "Share"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButtom extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButtom({super.key, required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.blue, size: 30),
+        Text(text, style: TextStyle(color: Colors.blue)),
+      ],
     );
   }
 }
@@ -22,7 +66,7 @@ class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       child: Row(
         children: [
           Column(
