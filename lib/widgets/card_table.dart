cc4f8afd7b4ee029b7131240903d16d1eed6be7a
roque121007 <jesusalexanderroquetrejo90@gmail.com>
2025-06-05 18:19:09 -0600

    Agregar la clase CardTable con múltiples tarjetas y diseño de tabla

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -5,6 +5,100 @@ class CardTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.lightBlue,
+              icon: Icons.border_all,
+              text: "Geneal",
+            ),
+            _SingleCard(
+              color: Colors.deepPurpleAccent,
+              icon: Icons.car_rental,
+              text: "Transport",
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.purple,
+              icon: Icons.shopping_bag,
+              text: "Shopping",
+            ),
+            _SingleCard(
+              color: Colors.yellow[800]!,
+              icon: Icons.receipt,
+              text: "Bills",
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.blue,
+              icon: Icons.movie,
+              text: "Entertainment",
+            ),
+            _SingleCard(
+              color: Colors.greenAccent,
+              icon: Icons.local_grocery_store,
+              text: "Grosery",
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.lightBlue,
+              icon: Icons.border_all,
+              text: "Geneal",
+            ),
+            _SingleCard(
+              color: Colors.deepPurpleAccent,
+              icon: Icons.car_rental,
+              text: "Transport",
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _SingleCard extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String text;
+
+  const _SingleCard({
+    required this.icon,
+    required this.color,
+    required this.text,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      height: 180,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: color,
+            radius: 40,
+            child: Icon(icon, size: 40),
+          ),
+          SizedBox(height: 10),
+          Text(text, style: TextStyle(color: color, fontSize: 18)),
+        ],
+      ),
+    );
   }
 }
